91f6f8350de1bcd839eb0d4fc95cad6f0a16bc59
viniyr <vinicius.yonezawa@gmail.com>
2021-07-12 20:31:28 -0300

    Handling exceptions and clearing screen

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,7 +8,7 @@ public class Board {
 	
 	public Board(int rows, int columns) {
 		if (rows < 1 || columns < 1) { 
-			throw new BoardException("Error creating board: there must be at least 1 row and 1 column!");
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column! Press enter to continue!");
 		}
 		this.rows = rows;
 		this.columns = columns;
@@ -25,21 +25,21 @@ public class Board {
 	
 	public Piece piece(int row, int column) { 
 		if(!positionExists(row, column)) {
-			throw new BoardException("Position not on the board");
+			throw new BoardException("Position not on the board! Press enter to continue!");
 		}
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) { 
 		if(!positionExists(position)) {
-			throw new BoardException("Position not on the board");
+			throw new BoardException("Position not on the board! Press enter to continue!");
 		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) { 
 		if (thereIsAPiece(position)) { 
-			throw new BoardException("There is already a piece on position " + position);
+			throw new BoardException("There is already a piece on position " + position + " Press enter to continue!\"");
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
@@ -47,7 +47,7 @@ public class Board {
 	
 	public Piece removePiece(Position position) { 
 		if (!positionExists(position)) { 
-			throw new BoardException("Position not on the board");
+			throw new BoardException("Position not on the board, press enter to continue!");
 		}
 		if (piece(position) == null) { 
 			return null;
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -42,7 +42,7 @@ public class ChessMatch {
 	
 	private void validateSourcePosition(Position position) { 
 		if (!board.thereIsAPiece(position)) { 
-			throw new ChessException("There is no piece on source position");
+			throw new ChessException("There is no piece on source position, press enter to continue!");
 		}
 	}
 	
